fe59be7d7e92213a6dab634b899fdfa6e628ad22
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2017-01-03 23:15:49 -0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nathon Scott
                 </div>
 
                 <div class="links">
