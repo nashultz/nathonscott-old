20a5ae3bc441a24ec5c6af34096b88ea092160e7
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2017-01-05 22:09:33 -0600

    added bootstrap framework

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -30,7 +30,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <!-- Styles -->
-
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
 <body>
@@ -141,6 +141,6 @@
     <!-- --------------------------------------------------------------------------------
         ----- JAVASCRIPT -----
     --------------------------------------------------------------------------------- -->
-    <script src="themes/default/assets/js/all.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
