9786cd426b3b558d207871b6bbc8b2575392207c
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2017-01-17 13:07:32 -0600

    testing vue

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -145,19 +145,6 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
     <script src="/js/app.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script>
-        // jQuery for page scrolling feature - requires jQuery Easing plugin
-        $(function() {
-            $('body').on('click', '.page-scroll a', function(event) {
-                var $anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
-                }, 1500, 'easeInOutExpo');
-                event.preventDefault();
-            });
-        });
-    </script>
 
 </body>
 </html>
